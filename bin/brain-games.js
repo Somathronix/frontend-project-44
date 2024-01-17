60adfbe853f76b67922ca6254907905689e758cd
Somathronix <davozfire@gmail.com>
2024-01-17 20:25:21 +0700

    for tutor

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-import greetUser from '../src/cli';
+import greetUser from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greetUser();
